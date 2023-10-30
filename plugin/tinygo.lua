7c35a3c18adf31f98451ce0b77f372f3099551fd
Pablo Collado Soto <pcolladosoto@gmx.com>
2023-10-30 10:28:22 +0100

    Remove the plugin directory
    We don't really have to do anything automatically at startup.
    The call to `setup()` takes care of everything!

--- plugin/tinygo.lua
+++
@@ -1,9 +0,0 @@
--- Check we havent' loaded the plugin already...
-if vim.g.loaded_tinygo == 1 then return end
-vim.g.loaded_telescope = 1
-
--- Load our Lua codebase
-local ok, tinygo = pcall(require, "tinygo")
-if not ok then
-	vim.print("couldn't find the tinygo codebase!")
-end
